c32bc71fdc74d36d15817effcf96a23f7416462f
Kevin Wang <kevmo314@gmail.com>
2026-08-08 05:31:14 -0400

    Piggyback the library kernel table on cuLibraryLoadData responses (#467)

--- codegen/annotations.h
+++ codegen/annotations.h
@@ -2463,6 +2463,7 @@ CUresult cuLibraryLoadFromFile(CUlibrary *library, const char *fileName,
  */
 CUresult cuLibraryUnload(CUlibrary library);
 /**
+ * @disabled client - manual client serves the library kernel table
  * @routingkey LIBRARY library
  * @param pKernel RECV_ONLY
  * @param library SEND_ONLY
