82a3a1eda3e1a5299c87f159e4bd10ba02a3717c
Kevin Wang <kevmo314@gmail.com>
2026-09-11 00:26:02 -0400

    Return library metadata with load responses to hide startup latency (#747)

--- codegen/annotations_cuda.h
+++ codegen/annotations_cuda.h
@@ -3317,10 +3317,6 @@ void lupineFunctionParamLayoutSnapshot();
 /** @disabled */
 void lupineFunctionAttributeSnapshot();
 /** @disabled */
-void lupineLibrarySnapshot();
-/** @disabled */
-void lupineLibraryAttributeSnapshot();
-/** @disabled */
 void cuGraphConditionalHandleCreate();
 /** @disabled handle_cuGraphAddNode */
 void cuGraphAddNode_v2();
